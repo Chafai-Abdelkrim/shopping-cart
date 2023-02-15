6f68c161fee451a6dc36250b31f8b7fb596d1c6c
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-02-15 09:31:27 +0100

    created state and add and remove to cart functions

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,36 @@ import { gameList } from './api';
 import { Home, GameList, GameDetails, NotFound } from './pages';
 import getPrice from './utils/getPrice';
 import { Game } from './types/Game.types';
-import './styles/App.css'
+import './styles/App.css';
 
 const loadGames = async (search = '') => {
-    const response = await gameList({ page_size: 50, search });
+  const response = await gameList({ page_size: 50, search });
 
-    let { results } = response;
-    results = results.filter(game => game.rating_count > (search ? 50 : 10));
-    results.forEach(game => game.price = getPrice(game));
-}
+  let { results } = response;
+  results = results.filter((game) => game.rating_count > (search ? 50 : 10));
+  results.forEach((game) => (game.price = getPrice(game)));
+
+  return results;
+};
 
 function App() {
+  const [cartItems, setCartItems] = useState<Game[]>([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const location = useLocation();
 
+  const addToCart = useCallback(
+    (game: Game) => {
+      setCartItems([...cartItems, game]);
+    },
+    [cartItems]
+  );
+
+  const removeFromCart = useCallback(
+    (ids: number[]) => {
+      setCartItems(cartItems.filter((item) => !ids.includes(item.id)));
+    },
+    [cartItems]
+  );
 }
 
 export default App;
