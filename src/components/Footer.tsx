40f4132792bf4d510c48fda1ec0e05b21e2f375e
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-02-17 09:32:13 +0100

    added Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import Github from './Github';
+
+function Footer() {
+  return (
+    <footer>
+      <Github username="Chafai-Abdelkrim" />
+      <a
+        className="Button"
+        href="https://rawg.io/apidocs"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        RAWG API
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
