1ee839af772f765dde5fec539a47a35cd5c428a7
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-02-17 09:26:47 +0100

    added github component

---
+++ src/components/Github.tsx
@@ -0,0 +1,18 @@
+import { RiGithubLine } from 'react-icons/ri';
+
+function Github({ username }: { username: string }) {
+    const nameArray = username.split('-');
+
+  return (
+    <a
+      href={`https://github.com/${username}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="Github Button"
+      title="Check Out My Github"
+    >
+      <RiGithubLine />
+      {`${nameArray[1]} ${nameArray[0]}}`}
+    </a>
+  );
+}
